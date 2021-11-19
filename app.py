6f6f78e9b8ce58a9ac7026b30607b8aebe741bb4
Mark McQuade <mark@newagetechie.com>
2021-11-19 11:21:37 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+
+from newspaper import Article
+from newspaper import Config
+import gradio as gr
+from gradio.mix import Parallel, Series
+
+def extrac_text(url):
+  USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0'
+  config = Config()
+  config.browser_user_agent = USER_AGENT
+  config.request_timeout = 10
+  
+  article = Article(url, config=config)
+  article.download()
+  article.parse()
+  text = article.text
+  return text
+
+extractor = gr.Interface(extrac_text, 'text', 'text')
+summarizer = gr.Interface.load("mmcquade11/autonlp-reuters-summarization-34018133")
+sample_url = [['https://www.cp24.com/news/ontario-reports-481-new-covid-19-cases-1-death-1.5667950'],
+]
+desc =  '''
+         The news summarizer app uses an AutoNLP model trained on the Reuters dataset.
+        '''
+iface = Series(extractor, summarizer, 
+  inputs = gr.inputs.Textbox(
+      lines = 2,
+      label = 'Enter URL below'
+  ),
+  outputs = 'text',
+  title = 'News Summarizer',
+  theme = 'grass',
+  layout = 'horizontal',
+  description = desc,
+  examples=sample_url)
+iface.launch()
